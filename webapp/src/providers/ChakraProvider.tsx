1530166fcbdf034c28a3e3356e7cc2a7b352db06
Travis <146148+rockacola@users.noreply.github.com>
2026-04-04 11:12:54 +1100

    Move webapp out of this repo

--- webapp/src/providers/ChakraProvider.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ChakraProvider as BaseChakraProvider, defaultSystem } from '@chakra-ui/react';
-
-export function ChakraProvider({ children }: { children: React.ReactNode }) {
-  return <BaseChakraProvider value={defaultSystem}>{children}</BaseChakraProvider>;
-}
